5c614b41e598bdd1537b69ad5a35446c13d3aeea
Stuart Glaser <stuglaser@gmail.com>
2022-12-09 22:21:28 -0800

    day 10, 6 us

---
+++ src/day10.rs
@@ -0,0 +1,250 @@
+const DAY: i32 = 10;
+
+enum Op {
+    AddX(i32),
+    Noop,
+}
+
+fn parse_instruction(line: &str) -> Op {
+    let mut pieces = line.split(" ");
+    match pieces.next().unwrap() {
+        "noop" => Op::Noop,
+        "addx" => Op::AddX(pieces.next().unwrap().parse().unwrap()),
+        _ => unreachable!(),
+    }
+}
+
+pub fn day10(test_mode: bool, print: bool) {
+    let file_str = std::fs::read_to_string(format!("inputs/input{:02}.txt", DAY)).unwrap();
+    let input_str = if test_mode {
+        TEST_EXAMPLE
+    } else {
+        &file_str.trim_end()
+    };
+
+    let program: Vec<Op> = input_str.lines().map(parse_instruction).collect();
+
+    let mut cycle: usize = 1;
+    let mut x: i32 = 1;
+
+    let mut part1 = 0;
+    let mut screen = [0u8; 6 * 40];
+    for instr in &program {
+        // How long to wait.
+        let cycles_to_wait = match instr {
+            Op::AddX(_) => 2,
+            Op::Noop => 1,
+        };
+
+        // Wait
+        for _ in 0..cycles_to_wait {
+            if (cycle + 20) % 40 == 0 {
+                part1 += cycle as i32 * x;
+            }
+
+            let screen_position: usize = cycle - 1;
+            if (x - (screen_position as i32) % 40).abs() <= 1 {
+                screen[screen_position] = 1;
+            }
+
+            cycle += 1;
+        }
+
+        // Execute instruction
+        match instr {
+            Op::AddX(value) => x += value,
+            Op::Noop => (),
+        }
+    }
+
+    if print {
+        println!("Day {}.  Part 1: {}", DAY, part1);
+    }
+    assert_eq!(part1, if test_mode { 13140 } else { 14720 });
+
+    if print {
+        println!("Day {}.  Part 2:", DAY);
+        for i in 0..6 {
+            println!(
+                "{}",
+                &screen[(i * 40)..((i + 1) * 40)]
+                    .iter()
+                    .map(|k| if *k > 0 { '#' } else { '.' })
+                    .collect::<String>()
+            );
+        }
+    }
+
+    #[rustfmt::skip]
+    const PART2_TEST_ANSWER: [u8; 6*40] = [
+        1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 
+        1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 
+        1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 
+        1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 
+        1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 
+        1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0];
+    #[rustfmt::skip]
+    const PART2_ANSWER: [u8; 6*40] = [
+        1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 
+        1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 
+        1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 
+        1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 
+        1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 
+        1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0];
+    // FZBPBFZF
+    assert_eq!(
+        screen,
+        if test_mode {
+            PART2_TEST_ANSWER
+        } else {
+            PART2_ANSWER
+        }
+    );
+}
+
+const TEST_EXAMPLE: &'static str = "addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
