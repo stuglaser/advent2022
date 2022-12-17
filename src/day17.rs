a837790e8fc4fca994902fd1b68e658cf0a9be48
Stuart Glaser <stuglaser@gmail.com>
2022-12-17 14:12:18 -0800

    WIP saving before I let the state be offset.

--- src/day17.rs
+++ src/day17.rs
@@ -1,5 +1,7 @@
 use std::{cmp::max, iter::zip};
 
+use rustc_hash::FxHashMap;
+
 use crate::utils::{minmax, Grid};
 
 const DAY: i32 = 17;
@@ -46,96 +48,13 @@ fn extend_rows_to(grid: &mut Grid<u8>, rows: usize, value: u8) {
     grid.rows = rows;
 }
 
-fn collides(elev: &[usize], rock: &Grid<u8>, r: i32, c: i32) -> bool {
-    if r < 0 || c < 0 || c as usize + rock.cols > elev.len() {
-        // Collides with edges
-        return true;
-    }
-
-    for rock_r in 0..rock.rows {
-        for rock_c in 0..rock.cols {
-            if rock[(rock_r, rock_c)] > 0 {
-                if r as usize + rock_r < elev[c as usize + rock_c] {
-                    // Collides with previous pieces
-                    return true;
-                }
-            }
-        }
-    }
-    false
-}
-
-fn blit(elev: &mut [usize], rock: &Grid<u8>, r: usize, c: usize, val: u8) {
-    for rock_r in 0..rock.rows {
-        for rock_c in 0..rock.cols {
-            if rock[(rock_r, rock_c)] > 0 {
-                let this_elev = r + rock_r + 1;
-                elev[c as usize + rock_c] = max(elev[c as usize + rock_c], this_elev);
-            }
-        }
-    }
-}
-
-fn fmt_elev(elev: &[usize]) -> String {
-    let (min_elev, max_elev) = minmax(elev.iter()).unwrap();
-    let mut out = String::with_capacity((elev.len() + 1) * (max_elev - min_elev + 1));
-
-    out.push_str(&format!("{:?}\n", elev));
-
-    let mut row = *max_elev;
-    loop {
-        for e in elev.iter() {
-            out.push(if *e > row { '#' } else { '·' });
-        }
-
-        out.push('\n');
-        if row == 0 || row == *min_elev {
-            break;
-        }
-        row -= 1;
-    }
-    out
-}
-
-fn visualize(elev: &[usize], rock: &Grid<u8>, rock_r: usize, rock_c: usize) -> String {
-    let (min_elev, max_elev) = minmax(elev.iter()).unwrap();
-    let max_row = max(*max_elev, rock_r + rock.rows);
-
-    let mut out = String::with_capacity((elev.len() + 1) * (max_row - min_elev + 1));
-    for row in (*min_elev..(max_row + 1)).rev() {
-        for col in 0..elev.len() {
-            let elev_overlaps = elev[col] > row;
-            let rock_overlaps = rock_r <= row
-                && row < rock_r + rock.rows
-                && rock_c <= col
-                && col < rock_c + rock.cols
-                && rock[(row - rock_r, col - rock_c)] > 0;
-            assert!(!(elev_overlaps && rock_overlaps));
-
-            out.push(if elev_overlaps {
-                '#'
-            } else if rock_overlaps {
-                '@'
-            } else {
-                '·'
-            });
-        }
-
-        out.push('\n');
-    }
-    out
-}
+const COLORS: [char; 6] = ['·', '#', '@', '▒', '◉', '▢'];
 
 fn fmt_cave(cave: &Grid<u8>) -> String {
     let mut out = String::with_capacity(cave.rows * (cave.cols + 1));
     for r in (0..cave.rows).rev() {
         for c in 0..cave.cols {
-            out.push(match cave[(r, c)] {
-                0 => '·',
-                1 => '#',
-                2 => '@',
-                _ => unreachable!(),
-            });
+            out.push(COLORS[cave[(r, c)] as usize]);
         }
         out.push('\n');
     }
@@ -143,17 +62,42 @@ fn fmt_cave(cave: &Grid<u8>) -> String {
 }
 
 struct State {
-    // cave: Grid<u8>,
-    elev: [usize; 7],
+    cave: Grid<u8>,
+    bottom: usize,
     top: usize,
 }
 
 impl State {
     fn new() -> Self {
         Self {
-            // cave: Grid::filled(1, 7, 0),
-            elev: [0; 7],
+            cave: Grid::filled(1, 7, 0),
+            bottom: 0,
             top: 0,
+        }
+    }
+}
+
+
+fn collides(cave: &Grid<u8>, rock: &Grid<u8>, r: i32, c: i32) -> bool {
+    if r < 0 || c < 0 || c as usize + rock.cols > cave.cols {
+        return true;
+    }
+    for rock_r in 0..rock.rows {
+        for rock_c in 0..rock.cols {
+            if rock[(rock_r, rock_c)] > 0 && cave[(r as usize + rock_r, c as usize + rock_c)] > 0 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn blit(cave: &mut Grid<u8>, rock: &Grid<u8>, r: usize, c: usize, val: u8) {
+    for rock_r in 0..rock.rows {
+        for rock_c in 0..rock.cols {
+            if rock[(rock_r, rock_c)] > 0 {
+                cave[(r as usize + rock_r, c as usize + rock_c)] = val;
+            }
         }
     }
 }
@@ -167,36 +111,41 @@ fn drop_rocks<'a, RockIt, BlowIt>(
     RockIt: Iterator<Item = &'a Grid<u8>>,
     BlowIt: Iterator<Item = i8>,
 {
+    state.cave.data.reserve(7 * num_rocks * 3);
     for i in 0..num_rocks {
-        let mut drop_c = 2;
-        let mut drop_r = state.top as i32 + 3;
-
-        // println!("[{i}] Elevation:\n{}", fmt_elev(&state.elev));
+        let mut drop_c: i32 = 2;
+        let mut drop_r: i32 = state.top as i32 + 3;
 
         let rock = rock_iter.next().unwrap();
-        // println!("Visualize:\n{}", visualize(&state.elev, rock, drop_r as usize, drop_c as usize));
+
+        extend_rows_to(&mut state.cave, drop_r as usize + rock.rows, 0);
+
         loop {
             // Sideways
             let blow = blow_iter.next().unwrap();
-            if !collides(&state.elev, rock, drop_r, drop_c + blow as i32) {
+            if !collides(&state.cave, &rock, drop_r, drop_c + blow as i32) {
                 drop_c += blow as i32;
             }
-            // println!("[{i}] blown to\n{}", visualize(&state.elev, rock, drop_r as usize, drop_c as usize));
 
             // Down
-            if drop_r == 0 || collides(&state.elev, rock, drop_r - 1, drop_c) {
+            if drop_r >= 0 && collides(&state.cave, &rock, drop_r - 1, drop_c) {
                 // Places the rock
-                blit(&mut state.elev, rock, drop_r as usize, drop_c as usize, 1);
+                // blit(&mut state.cave, &rock, drop_r as usize, drop_c as usize, 1);
+                blit(&mut state.cave, &rock, drop_r as usize, drop_c as usize, (i % (COLORS.len() - 1) + 1) as u8);
                 state.top = max(state.top, drop_r as usize + rock.rows);
+
+                // println!("[{i}] Placed:\n{}", fmt_cave(&state.cave));
                 break;
             } else {
                 drop_r -= 1;
-            }
 
-            // println!("[{i}] dropped to\n{}", visualize(&state.elev, rock, drop_r as usize, drop_c as usize));
+            }
         }
+        // println!("{}", fmt_cave(&cave));
     }
 }
+
+
 
 pub fn day17(test_mode: bool, print: bool) {
     let file_str = std::fs::read_to_string(format!("inputs/input{:02}.txt", DAY)).unwrap();
@@ -243,11 +192,63 @@ pub fn day17(test_mode: bool, print: bool) {
         &mut rock_iter.clone(),
         &mut blow_iter.clone(),
     );
-    println!(
-        "Looping part is {} high. Approx full height = {}",
-        state_repeated.top,
-        state_repeated.top * (1000000000000 / looping_after)
-    );
+    println!("Looping part is {} high. Approx full height = {}", state_repeated.top, state_repeated.top * (1000000000000 / looping_after));
+
+    let mut state = State::new();
+
+    // Really we should search for a full blockage, but probably this value is big enough.
+    const ASSUME_TRIMMABLE: usize = 100;
+
+    fn clone_rows(grid: &Grid<u8>, start: usize, end: usize) -> Grid<u8> {
+        Grid{
+            rows: end - start,
+            cols: grid.cols,
+            data: grid.data[(start * grid.cols)..(end * grid.cols)].to_vec()}
+    }
+
+    fn hash_occupancy(grid: &Grid<u8>) -> usize {
+        let mut hash: usize = 432086524387577;
+        for d in &grid.data {
+            if *d > 0 {
+                hash = hash.overflowing_mul(8274393).0;
+                hash = hash.overflowing_add(91832751).0;
+            } else {
+                hash = hash.overflowing_mul(22971).0;
+                hash = hash.overflowing_add(993777).0;
+            }
+        }
+        hash
+    }
+
+    struct Effect {
+        crown_after: Grid<u8>,
+        crown_hash_after: usize,
+    }
+    let mut successor = FxHashMap::<usize, Effect>::with_capacity_and_hasher(128, Default::default());
+
+    let mut last_crown_hash = 0;
+    for k in 0..1000 {
+        let last_top = state.top;
+        drop_rocks(&mut state, looping_after, &mut rock_iter, &mut blow_iter);
+
+        if successor.contains_key(&last_crown_hash) {
+            println!("LOOPING!!!!! after {k}");
+            return;
+        }
+
+        println!("Round {k}, increased by {}:\n{}", state.top - last_top, fmt_cave(&state.cave));
+
+        let crown = clone_rows(&state.cave, state.top - ASSUME_TRIMMABLE, state.top);
+        let crown_hash = hash_occupancy(&crown);
+        println!("Crown is: {}\n{}", crown_hash, fmt_cave(&crown));
+        // println!("[{k}] increased by {}", state.top - last_top);
+        successor.insert(last_crown_hash, Effect{
+            crown_after: crown,
+            crown_hash_after: crown_hash,
+        });
+
+        last_crown_hash = crown_hash;
+    }
 
     // println!("{}", fmt_cave(&state_repeated.cave));
 
@@ -259,73 +260,3 @@ pub fn day17(test_mode: bool, print: bool) {
 }
 
 const TEST_EXAMPLE: &'static str = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
-
-// No longer needed, since the elevation method is more efficient,
-// but I liked this code, so I kept it around.
-//
-// fn collides(cave: &Grid<u8>, rock: &Grid<u8>, r: i32, c: i32) -> bool {
-//     if r < 0 || c < 0 || c as usize + rock.cols > cave.cols {
-//         return true;
-//     }
-//     for rock_r in 0..rock.rows {
-//         for rock_c in 0..rock.cols {
-//             if rock[(rock_r, rock_c)] > 0 && cave[(r as usize + rock_r, c as usize + rock_c)] > 0 {
-//                 return true;
-//             }
-//         }
-//     }
-//     false
-// }
-//
-// fn blit(cave: &mut Grid<u8>, rock: &Grid<u8>, r: usize, c: usize, val: u8) {
-//     for rock_r in 0..rock.rows {
-//         for rock_c in 0..rock.cols {
-//             if rock[(rock_r, rock_c)] > 0 {
-//                 cave[(r as usize + rock_r, c as usize + rock_c)] = val;
-//             }
-//         }
-//     }
-// }
-//
-// fn drop_rocks<'a, RockIt, BlowIt>(
-//     state: &mut State,
-//     num_rocks: usize,
-//     rock_iter: &mut RockIt,
-//     blow_iter: &mut BlowIt,
-// ) where
-//     RockIt: Iterator<Item = &'a Grid<u8>>,
-//     BlowIt: Iterator<Item = i8>,
-// {
-//     state.cave.data.reserve(7 * num_rocks * 3);
-//     for i in 0..num_rocks {
-//         let mut drop_c: i32 = 2;
-//         let mut drop_r: i32 = state.top as i32 + 3;
-//
-//         let rock = rock_iter.next().unwrap();
-//
-//         if i % 1_000_000 == 0 {
-//             println!("[{}] Dropped from {}, {}", i + 1, drop_r, drop_c);
-//         }
-//         extend_rows_to(&mut state.cave, drop_r as usize + rock.rows, 0);
-//
-//         loop {
-//             // Sideways
-//             let blow = blow_iter.next().unwrap();
-//             if !collides(&state.cave, &rock, drop_r, drop_c + blow as i32) {
-//                 drop_c += blow as i32;
-//             }
-//
-//             // Down
-//             if drop_r >= 0 && collides(&state.cave, &rock, drop_r - 1, drop_c) {
-//                 // Place the rock
-//                 blit(&mut state.cave, &rock, drop_r as usize, drop_c as usize, 1);
-//                 state.top = max(state.top, drop_r as usize + rock.rows);
-//                 break;
-//             } else {
-//                 drop_r -= 1;
-//
-//             }
-//         }
-//         // println!("{}", fmt_cave(&cave));
-//     }
-// }
